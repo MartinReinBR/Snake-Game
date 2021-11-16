43238fa68baa276e4ebdbd95492d6f6a9a72c165
MartinReinBR <martin.blomquist@hotmail.com>
2021-11-16 15:05:00 +0100

    Added unity project

---
+++ My_Snake_Game/Assets/Scripts/Fruit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject spawner;
+
+    private void Start()
+    {
+        spawner = GameObject.FindWithTag("Spawner");
+        if (spawner == null)
+        {
+            Debug.Log("error");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            spawner.GetComponent<FruitSpawner>().SpawnFruit();
+            Destroy(gameObject);
+        }
+    }
+}
